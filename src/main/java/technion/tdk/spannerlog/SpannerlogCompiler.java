423915cef9a2e3b87f9a83a0e22dab453755c005
Yoav Nahshon <yoavn@cs.technion.ac.il>
2016-10-19 00:39:10 +0300

    Fixing bugs before running spouse on big input

--- src/main/java/technion/tdk/spannerlog/SpannerlogCompiler.java
+++ src/main/java/technion/tdk/spannerlog/SpannerlogCompiler.java
@@ -19,6 +19,7 @@ class SpannerlogCompiler {
                 .stream()
                 .filter(s -> s instanceof IEFunctionSchema)
                 .map(s -> (IEFunctionSchema) s)
+                .filter(s -> s.getInputAtoms() != null)
                 .collect(Collectors.toMap(IEFunctionSchema::getName, this::compile));
     }
 
